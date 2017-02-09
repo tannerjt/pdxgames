990299d1090af5014d547c76b4f1b491120f3459
tannerjt <biolsurfer85@gmail.com>
2017-02-08 20:12:06 -0800

    Add categories page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209034426) do
+ActiveRecord::Schema.define(version: 20170209034513) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -24,8 +24,10 @@ ActiveRecord::Schema.define(version: 20170209034426) do
   create_table "photos", force: :cascade do |t|
     t.string   "image"
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_photos_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
