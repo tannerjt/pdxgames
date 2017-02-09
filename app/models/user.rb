5212c76b84b91141b9b29101470a01347a594e10
tannerjt <biolsurfer85@gmail.com>
2017-02-08 19:46:04 -0800

    Add foreign keys

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  has_many :categories
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209032755) do
+ActiveRecord::Schema.define(version: 20170209034426) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
     t.string   "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "photos", force: :cascade do |t|
